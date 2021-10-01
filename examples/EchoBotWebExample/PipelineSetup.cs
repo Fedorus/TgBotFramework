fd6304c54e282c64378d1e136005c43ff3dec62c
Sinys <alex.fedorus@gmail.com>
2021-10-02 00:21:45 +0300

    Comments in PipelineSetup.cs MemberAddedEcho.cs moved

--- examples/EchoBotWebExample/PipelineSetup.cs
+++ examples/EchoBotWebExample/PipelineSetup.cs
@@ -1,12 +1,9 @@
-using System.Threading;
-using System.Threading.Tasks;
 using EchoBotProject;
 using EchoBotProject.Commands;
 using EchoBotProject.EchoHandlers;
 using EchoBotProject.Handlers;
 using EchoBotProject.States;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using TgBotFramework;
 using TgBotFramework.UpdatePipeline;
 
@@ -14,6 +11,10 @@ namespace EchoBotWebExample
 {
     public static class PipelineSetup
     {
+        /// <summary>
+        /// Since Startup file may be huge and its hard to keep track whats added to DI it may be better to extract it to such method
+        /// </summary>
+        /// <returns></returns>
         public static IServiceCollection ServicesForExamplePipelineBuilder(this IServiceCollection services)
         {
             services.AddScoped<MessageHandler>();
@@ -30,6 +31,10 @@ namespace EchoBotWebExample
             return services;
         }
 
+        /// <summary>
+        /// Same with pipeline ending
+        /// </summary>
+        /// <returns></returns>
         public static IBotPipelineBuilder<BotExampleContext> ExamplePipelineBuilder
             (this IBotPipelineBuilder<BotExampleContext> pipe)
             => pipe
@@ -63,21 +68,5 @@ namespace EchoBotWebExample
                 // fired when user blocks/unblocks bot, adds user to chat
                 .MapWhen<BotWasBlockedUnblockedOrAdded>(On.MyChatMember)
                 .MapWhen<GroupChatUserUpdated>(On.ChatMember);
-    }
-
-    public class MemberAddedEcho : IUpdateHandler<BotExampleContext>
-    {
-        private readonly ILogger<MemberAddedEcho> _logger;
-
-        public MemberAddedEcho(ILogger<MemberAddedEcho> logger)
-        {
-            _logger = logger;
-        }
-        
-        public async Task HandleAsync(BotExampleContext context, UpdateDelegate<BotExampleContext> next, CancellationToken cancellationToken)
-        {
-            var item = context.Update.Message;
-            _logger.LogInformation("UserOrBot {0} was added to chat {1} by {2}", item.NewChatMembers[0], item.Chat, item.From);
-        }
     }
 }
